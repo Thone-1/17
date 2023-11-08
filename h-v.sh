8facc619806740cecc49a1f748e47463d29f1b97
Thone-1 <150073849+Thone-1@users.noreply.github.com>
2023-11-08 23:46:13 +0800

    Update h-v.sh

--- h-v.sh
+++ h-v.sh
@@ -166,7 +166,7 @@ backend_docking_set(){
         check_docker
 	xrayr_file
 	rulelist_file
-	docker run --restart=always --name 17xrayr -d -v /usr/local/17xrayr/config.yml:/etc/XrayR/config.yml -v /usr/local/17xrayr/rulelist:/etc/XrayR/rulelist --network=host crackair/xrayr:latest
+	docker run --restart=always --name 17xrayr -d -v /usr/local/17xrayr/config.yml:/etc/XrayR/config.yml -v /usr/local/17xrayr/rulelist:/etc/XrayR/rulelist --network=host thone01/17xrayr-reality:v3
         setenforce 0
         sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
         greenbg "恭喜您，后端节点已搭建成功"
